fe0ac768296174133b2e7fbac597b80907d54a71
AntoineD28 <antoine.dumas3@etu.univ-lyon.fr>
2022-10-23 12:18:07 +0200

    init application spring

---
+++ CineamEpulSpring/src/test/java/com/epulCinema/CineamEpulSpring/CineamEpulSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epulCinema.CineamEpulSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineamEpulSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
