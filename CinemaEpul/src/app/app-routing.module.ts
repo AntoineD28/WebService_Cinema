7fc1da0815bde88d3ed69f19a92f298ead15d83c
AntoineD28 <antoine.dumas3@etu.univ-lyon.fr>
2022-11-23 18:30:34 +0100

    Card film

--- CinemaEpul/src/app/app-routing.module.ts
+++ CinemaEpul/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListeFilmsComponent } from './liste-films/liste-films.component';
 
 const routes: Routes = [];
 
--- CinemaEpul/src/app/app.module.ts
+++ CinemaEpul/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListeFilmsComponent } from './liste-films/liste-films.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeFilmsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'films', component: ListeFilmsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
